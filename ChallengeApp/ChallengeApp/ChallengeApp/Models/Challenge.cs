09d4e413a005282584907b4204093e5c7223a8d6
Juan Jose Muñoz Franco <disk737@hotmail.com>
2017-10-26 23:10:54 -0500

    ListChallengeView V1

---
+++ ChallengeApp/ChallengeApp/ChallengeApp/Models/Challenge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeApp.Models
+{
+    public class Challenge
+    {
+        public string ChallengeName { get; set; }
+        public string ChallengePoint { get; set; }
+    }
+}
---
+++ ChallengeApp/ChallengeApp/ChallengeApp/Views/ListChallengeView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using ChallengeApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChallengeApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListChallengeView : ContentPage
+    {
+        public ListChallengeView()
+        {
+            InitializeComponent();
+
+            ListChallenge.ItemsSource = new List<Challenge>
+            {
+                new Challenge{ChallengeName = "Explore", ChallengePoint ="5"},
+                new Challenge{ChallengeName ="Read", ChallengePoint = "3"},
+                new Challenge{ChallengeName = "Demostrate", ChallengePoint = "8"}
+            };
+
+            UserInfo userInfo = new UserInfo { UserPoints = "25" };
+
+            LabelPoints.Text = String.Format("Points: {0}", userInfo.UserPoints);
+
+        }
+    }
+}
